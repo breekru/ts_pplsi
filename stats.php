527059a29bbe7bb8ab6051dc8d5ef77528162ab6
breekru <breekru@gmail.com>
2022-06-28 16:23:10 -0500

    test

--- stats.php
+++ stats.php
@@ -32,7 +32,8 @@
     </tr>
     <tr>
         <td>Abandoned Calls</td>
-        <td class="bad">28</td><p><--Had a major change that held up queues with so many calling in at once</p>
+        <td class="bad">28</td>
+        <td><--Had a major change that held up queues with so many calling in at once</td>
     </tr>
     <tr>
         <td>Calls Answered</td>
