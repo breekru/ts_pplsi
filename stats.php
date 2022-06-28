6fbcac63abf00362ee67f066c319283dbd184204
breekru <breekru@gmail.com>
2022-06-28 14:07:29 -0500

    build

---
+++ stats.php
@@ -0,0 +1,66 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>PPLSI Technical Support Stats</title>
+  <meta name="description" content="Technical Support Stats">
+  <meta name="author" content="Brett Krumholz">
+
+  <meta property="og:title" content="Technical Support Stats">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://www.sitepoint.com/a-basic-html5-template/">
+  <meta property="og:description" content="Technical Support Stats">
+  <meta property="og:image" content="image.png">
+
+  <link rel="icon" href="img/favicon.ico">
+  <link rel="icon" href="img/favicon.svg" type="image/svg+xml">
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+  <link rel="stylesheet" href="css/form.css">
+
+</head>
+
+<body>
+<h1>6/20-6/26 Call Review</h1>
+<table>
+    <tr>
+        <td>Calls Received</td>
+        <td>404</td>
+    </tr>
+    <tr>
+        <td>Abandoned Calls</td>
+        <td>28</td>
+    </tr>
+    <tr>
+        <td>Calls Answered</td>
+        <td>376</td>
+    </tr>
+    <tr>
+        <td>Providers</td>
+        <td>176</td>
+    </tr>
+    <tr>
+        <td>Corporate</td>
+        <td>150</td>
+    </tr>
+    <tr>
+        <td>3rd Party Call Center</td>
+        <td>40</td>
+    </tr>
+    <tr>
+        <td>Vendors</td>
+        <td>3</td>
+    </tr>
+    <tr>
+        <td>Transfers from Front Desk</td>
+        <td>7</td>
+    </tr>
+</table>
+
+
+
+</body>
+</html>
