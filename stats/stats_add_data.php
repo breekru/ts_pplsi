2e795dde7b7546339f81637ef3df4c4f21f61e87
breekru <breekru@gmail.com>
2022-06-29 16:11:47 -0500

    test

--- stats/stats_add_data.php
+++ stats/stats_add_data.php
@@ -34,8 +34,8 @@ require('config.php');
 <body>
 <a href=stats_home.php>Return to Home Page</a>
 <h1>Add Data to the DB</h1>
-<form action="control_table.php" method="post">
-    <label for="timeframe">Month/Week/Day</label>
+<form action="control_table.php" method="post" class="center">
+    <label for="timeframe">Month/Week/Day</label><br>
     <select id="timeframe" name="timeframe">
         <option value="month">Month</option>
         <option value="week">Week</option>
