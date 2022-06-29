fb25a29c525679191a6eb1d3958cf0be6542c57a
breekru <breekru@gmail.com>
2022-06-29 10:22:16 -0500

    test

--- stats.php
+++ stats.php
@@ -36,11 +36,11 @@ require('config.php');
 
 <?php
 $count=1;
-$sel_query = "SELECT * FROM `stats`";
-//$sel_query="SELECT `id`, `timeframe`, `date`, `day_of_week`, `received`, `abandoned`, `answered`, `providers`, `corporate`, `call_centers`, `vendors`, `front_desk` FROM `stats` WHERE `id`= '1'";
+//$sel_query = "SELECT * FROM `stats`";
+$sel_query="SELECT `id`, `timeframe`, `date`, `day_of_week`, `received`, `abandoned`, `answered`, `providers`, `corporate`, `call_centers`, `vendors`, `front_desk` FROM `stats` WHERE `id`= '1'";
 $result = mysqli_query($con,$sel_query);
-//while($row = mysqli_fetch_assoc($result)) { 
-$row = mysqli_fetch_assoc($result);
+while($row = mysqli_fetch_assoc($result)) { 
+//$row = mysqli_fetch_assoc($result);
 ?>
 <table>
     <tr>
@@ -76,7 +76,7 @@ $row = mysqli_fetch_assoc($result);
         <td><?php echo $row["front_desk"]; ?>
     </tr>
     </table>
-    <?php //$count++; } ?>
+    <?php $count++; } ?>
 
 
 
