a35859e6240bbbb868a455fbd3c44bed9be540c7
breekru <breekru@gmail.com>
2022-06-29 13:01:28 -0500

    cleanup

--- stats/stats_day.php
+++ stats/stats_day.php
@@ -38,7 +38,6 @@ require('config.php');
 if(isset($_POST["usub"])){
     $date = $_POST["datepicker"];
 }
-echo $date;
 //$count=1;
 $sel_query = "SELECT * FROM `stats` WHERE `date`='$date'";
 //$sel_query = "SELECT * FROM `stats`";
@@ -47,7 +46,7 @@ $result = mysqli_query($con,$sel_query);
 //while($row = mysqli_fetch_assoc($result)) { 
 $row = mysqli_fetch_assoc($result);
 ?>
-<caption><h1>Weekly Call Data for <?php echo $row["day_of_week"]; ?> </h1></caption>
+<caption><h1>Weekly Call Data for <?php echo $row["day_of_week"]; echo $row["date"]; ?> </h1></caption>
     <tr>
         <td>Calls Received</td>
         <td><?php echo $row["received"]; ?></td>
