724828a73a8f41f8f1bfe00854a0d1920c0c3aed
breekru <breekru@gmail.com>
2022-06-29 13:02:12 -0500

    test

--- stats/stats_day.php
+++ stats/stats_day.php
@@ -46,7 +46,7 @@ $result = mysqli_query($con,$sel_query);
 //while($row = mysqli_fetch_assoc($result)) { 
 $row = mysqli_fetch_assoc($result);
 ?>
-<caption><h1>Weekly Call Data for <?php echo $row["day_of_week"]; echo $row["date"]; ?> </h1></caption>
+<caption><h1>Weekly Call Data for <?php echo $row["day_of_week "]; echo $row["date"]; ?> </h1></caption>
     <tr>
         <td>Calls Received</td>
         <td><?php echo $row["received"]; ?></td>
