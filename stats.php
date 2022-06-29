a79b7e7e6e80d9c17b64d99abb5d4a70e6cd669c
breekru <breekru@gmail.com>
2022-06-29 10:06:35 -0500

    test

--- stats.php
+++ stats.php
@@ -36,8 +36,8 @@ require('config.php');
 
 <?php
 $count=1;
-//$sel_query = "SELECT * FROM `stats`";
-$sel_query="SELECT `id`, `timeframe`, `date`, `day_of_week`, `received`, `abandoned`, `answered`, `providers`, `corporate`, `call_centers`, `vendors`, `front_desk` FROM `stats` WHERE `id`= '1'";
+$sel_query = "SELECT * FROM `stats`";
+//$sel_query="SELECT `id`, `timeframe`, `date`, `day_of_week`, `received`, `abandoned`, `answered`, `providers`, `corporate`, `call_centers`, `vendors`, `front_desk` FROM `stats` WHERE `id`= '1'";
 $result = mysqli_query($con,$sel_query);
 while($row = mysqli_fetch_assoc($result)) { 
 ?>
