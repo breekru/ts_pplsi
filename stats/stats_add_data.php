706eeb351b6a2c3bd4fe90675d65d616bc63ca84
breekru <breekru@gmail.com>
2022-07-21 14:11:47 -0500

    test

--- stats/stats_add_data.php
+++ stats/stats_add_data.php
@@ -36,7 +36,7 @@ require('config.php');
 
 </head>
 
-<body class="bg-secondary text-light">
+<body class="bg-grey-500 text-light">
   <div class="row">
     <div class="col-sm-4">
 <a class="text-light" href=stats_home.php>Return to Home Page</a>
