0380bce920688feea04429008ef01a143a3c1aa5
breekru <breekru@gmail.com>
2022-07-24 15:41:12 -0500

    week css update

--- stats/stats_week.php
+++ stats/stats_week.php
@@ -18,11 +18,27 @@
         <link rel="icon" href="img/favicon.ico">
         <link rel="icon" href="img/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-        <link rel="stylesheet" href="css/stats.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-dark-grey.css">
+        <!--<link rel="stylesheet" href="css/stats.css">-->
+
+        <style>
+            tr:nth-child(even) {
+                 background-color: #f1f1f1;
+                color: #616161;
+            }
+
+            tr:nth-child(odd) {
+                background-color: #616161;
+                color: #f1f1f1;
+            }
+        </style>
     </head>
-    <body>
+    <body class="w3-theme">
+        <div class="w3-row">
+        <div class="w3-col l12 w3-center">
         <a href=stats_home.php>Return to Home Page</a>
-        <table class="center">
+        <table class="w3-table w3-border w3-centered w3-card-4" style="width:100%; align=center;">
             <?php
                 if(isset($_POST["week"])){
                     $week = $_POST["weekpicker"];
@@ -45,6 +61,10 @@
                 <h2><?php echo $row_t["min"]; ?> - <?php echo $row_t["max"]; ?></h2>
 
             </caption>
+                </div>
+                <div class="w3-row-padding">
+                <div class="w3-col  l3 w3-center"><h4></h4></div>
+                <div class="w3-col l3 w3-center">
             <tr>
                 <td>Calls Received</td>
                 <td><?php echo $row["Received(week)"]; ?></td>
@@ -77,8 +97,118 @@
                 <td>Transfers from Front Desk</td>
                 <td><?php echo $row["Front Desk(week)"]; ?></td>
             </tr>
-        </table>
+        </table>        
         <?php //$count++; } ?>
-
+                </div>
+                <div class="w3-col l3 w3-center">
+            <!--<table class="center">-->
+            <table class="w3-table w3-border w3-centered w3-card-4" style="width:100%; align=center;">
+               <tr>
+                  <caption>
+                  <h2>Incomming calls By Hour</h2>
+                  <caption>
+               </tr>
+               <tr>
+                  <td>0000-0100</td>
+                  <td><?php echo $row["hour1"]; ?></td>
+               </tr>
+               <tr>
+                  <td>0100-0200</td>
+                  <td><?php echo $row["hour2"]; ?></td>
+               </tr>
+               <tr>
+                  <td>0200-0300</td>
+                  <td><?php echo $row["hour3"]; ?></td>
+               </tr>
+               <tr>
+                  <td>0300-0400</td>
+                  <td><?php echo $row["hour4"]; ?></td>
+               </tr>
+               <tr>
+                  <td>0400-0500</td>
+                  <td><?php echo $row["hour5"]; ?></td>
+               </tr>
+               <tr>
+                  <td>0500-0600</td>
+                  <td><?php echo $row["hour6"]; ?></td>
+               </tr>
+               <tr>
+                  <td>0600-0700</td>
+                  <td><?php echo $row["hour7"]; ?></td>
+               </tr>
+               <tr>
+                  <td>0700-0800</td>
+                  <td><?php echo $row["hour8"]; ?></td>
+               </tr>
+               <tr>
+                  <td>0800-0900</td>
+                  <td><?php echo $row["hour9"]; ?></td>
+               </tr>
+               <tr>
+                  <td>0900-1000</td>
+                  <td><?php echo $row["hour10"]; ?></td>
+               </tr>
+               <tr>
+                  <td>1000-1100</td>
+                  <td><?php echo $row["hour11"]; ?></td>
+               </tr>
+               <tr>
+                  <td>1100-1200</td>
+                  <td><?php echo $row["hour12"]; ?></td>
+               </tr>
+               <tr>
+                  <td>1200-1300</td>
+                  <td><?php echo $row["hour13"]; ?></td>
+               </tr>
+               <tr>
+                  <td>1300-1400</td>
+                  <td><?php echo $row["hour14"]; ?></td>
+               </tr>
+               <tr>
+                  <td>1400-1500</td>
+                  <td><?php echo $row["hour15"]; ?></td>
+               </tr>
+               <tr>
+                  <td>1500-1600</td>
+                  <td><?php echo $row["hour16"]; ?></td>
+               </tr>
+               <tr>
+                  <td>1600-1700</td>
+                  <td><?php echo $row["hour17"]; ?></td>
+               </tr>
+               <tr>
+                  <td>1700-1800</td>
+                  <td><?php echo $row["hour18"]; ?></td>
+               </tr>
+               <tr>
+                  <td>1800-1900</td>
+                  <td><?php echo $row["hour19"]; ?></td>
+               </tr>
+               <tr>
+                  <td>1900-2000</td>
+                  <td><?php echo $row["hour20"]; ?></td>
+               </tr>
+               <tr>
+                  <td>2000-2100</td>
+                  <td><?php echo $row["hour21"]; ?></td>
+               </tr>
+               <tr>
+                  <td>2100-2200</td>
+                  <td><?php echo $row["hour22"]; ?></td>
+               </tr>
+               <tr>
+                  <td>2200-2300</td>
+                  <td><?php echo $row["hour23"]; ?></td>
+               </tr>
+               <tr>
+                  <td>2300-0000</td>
+                  <td><?php echo $row["hour24"]; ?></td>
+               </tr>
+            </table>
+         </div>
+         <div class="w3-col l3"></div>
+      </div>
+      <?php //$count++; } ?>
+         
     </body>
 </html>
