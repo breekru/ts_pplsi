947e0a5047aadf1a6c03ec0ad3904ce1ebfad7bb
breekru <breekru@gmail.com>
2022-08-03 16:40:56 -0500

    Major change to getData

--- stats/getData.php
+++ stats/getData.php
@@ -1,19 +1,54 @@
 <?php
     require('config.php');
-
-    //fetch table rows from mysql db
-    $sql = "select * from stats";
-    $result = mysqli_query($con, $sql) or die("Error in Selecting " . mysqli_error($connection));
-
-    //create an array
-    $temparray = array();
-    while($row = mysqli_fetch_assoc($result))
-    {
-        $temparray[] = $row;
-    }
-    echo json_encode($temparray);
-
-    //close the db connection
-    mysqli_close($con);
 ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+ <meta charset="utf-8">
+ <title>TechJunkGigs</title>
+ <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ <script type="text/javascript">
+ google.load("visualization", "1", {packages:["corechart"]});
+ google.setOnLoadCallback(drawChart);
+ function drawChart() {
+ var data = google.visualization.arrayToDataTable([
 
+ ['day_of_week','received'],
+ <?php 
+			$query = "SELECT * from stats";
+
+			 $exec = mysqli_query($con,$query);
+			 while($row = mysqli_fetch_array($exec)){
+
+			 echo "['".$row['day_of_week']."',".$row['received']."],";
+			 }
+			 ?> 
+ 
+ ]);
+
+ var options = {
+ title: 'Number of Students according to their class',
+  pieHole: 0.5,
+          pieSliceTextStyle: {
+            color: 'black',
+          },
+          legend: 'none'
+ };
+ var chart = new google.visualization.PieChart(document.getElementById("columnchart12"));
+ chart.draw(data,options);
+ }
+	
+    </script>
+
+</head>
+<body>
+ <div class="container-fluid">
+ <div id="columnchart12" style="width: 100%; height: 500px;"></div>
+ </div>
+
+</body>
+</html>
